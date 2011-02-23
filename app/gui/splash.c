2c81fdafe150ecdac43d3514542766d05de02551
Michael Natterer <mitch@gimp.org>
2011-02-23 08:04:07 +0100

    app: use GdkRGBA instead of GdkColor in some places

--- app/gui/splash.c
+++ app/gui/splash.c
@@ -46,7 +46,7 @@ typedef struct
   gint            width;
   gint            height;
   GtkWidget      *progress;
-  GdkColor        color;
+  GdkRGBA         color;
   PangoLayout    *upper;
   gint            upper_x;
   gint            upper_y;
@@ -77,7 +77,7 @@ static void        splash_rectangle_union     (GdkRectangle   *dest,
                                                gint            offset_y);
 static void        splash_average_text_area   (GimpSplash     *splash,
                                                GdkPixbuf      *pixbuf,
-                                               GdkColor       *color);
+                                               GdkRGBA        *rgba);
 
 static GdkPixbufAnimation * splash_image_load          (gboolean     be_verbose);
 static GdkPixbufAnimation * splash_image_pick_from_dir (const gchar *dirname,
@@ -235,7 +235,7 @@ splash_area_draw (GtkWidget  *widget,
                   cairo_t    *cr,
                   GimpSplash *splash)
 {
-  gdk_cairo_set_source_color (cr, &splash->color);
+  gdk_cairo_set_source_rgba (cr, &splash->color);
 
   cairo_move_to (cr, splash->upper_x, splash->upper_y);
   pango_cairo_show_layout (cr, splash->upper);
@@ -316,7 +316,7 @@ splash_rectangle_union (GdkRectangle   *dest,
 static void
 splash_average_text_area (GimpSplash *splash,
                           GdkPixbuf  *pixbuf,
-                          GdkColor   *color)
+                          GdkRGBA    *color)
 {
   const guchar *pixels;
   gint          rowstride;
@@ -371,7 +371,8 @@ splash_average_text_area (GimpSplash *splash,
 
     }
 
-  color->red = color->green = color->blue = (luminance << 8 | luminance);
+  color->red = color->green = color->blue = (luminance << 8 | luminance) / 255.0;
+  color->alpha = 1.0;
 }
 
 static GdkPixbufAnimation *
