85a00e7c0437028f55bacc6e219a93c78e5afd7d
Michael Natterer <mitch@gimp.org>
2014-04-06 15:48:53 +0200

    app: first draft of GimpMybrushTool, which uses MyPaint brushes

--- app/tools/gimp-tools.c
+++ app/tools/gimp-tools.c
@@ -70,6 +70,7 @@
 #include "gimpmagnifytool.h"
 #include "gimpmeasuretool.h"
 #include "gimpmovetool.h"
+#include "gimpmybrushtool.h"
 #include "gimppaintbrushtool.h"
 #include "gimppenciltool.h"
 #include "gimpperspectiveclonetool.h"
@@ -142,6 +143,7 @@ gimp_tools_init (Gimp *gimp)
     gimp_perspective_clone_tool_register,
     gimp_heal_tool_register,
     gimp_clone_tool_register,
+    gimp_mybrush_tool_register,
     gimp_ink_tool_register,
     gimp_airbrush_tool_register,
     gimp_eraser_tool_register,
@@ -534,6 +536,10 @@ gimp_tools_register (GType                   tool_type,
   else if (tool_type == GIMP_TYPE_INK_TOOL)
     {
       paint_core_name = "gimp-ink";
+    }
+  else if (tool_type == GIMP_TYPE_MYBRUSH_TOOL)
+    {
+      paint_core_name = "gimp-mybrush";
     }
   else
     {
