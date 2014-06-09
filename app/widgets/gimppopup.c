43d7bba04e83c29ac948226d05b6a11f4d134782
Michael Natterer <mitch@gimp.org>
2014-06-10 00:15:55 +0200

    app: remove GTK_OBJECT() case in GimpPopup

--- app/widgets/gimppopup.c
+++ app/widgets/gimppopup.c
@@ -229,7 +229,7 @@ gimp_popup_key_press (GtkWidget   *widget,
   if (gtk_binding_set_activate (binding_set,
                                 kevent->keyval,
                                 kevent->state,
-                                GTK_OBJECT (widget)))
+                                G_OBJECT (widget)))
     {
       return TRUE;
     }
